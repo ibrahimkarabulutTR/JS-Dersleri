0b1aa8beb1d07f13803490f25bc918324fb7b2ee
İbrahim Karabulut <job.ibrahimkarabulut@gmail.com>
2023-05-02 17:21:38 +0300

    2-Degiskenler İslendi

---
+++ 002-Degiskenler/main.js
@@ -0,0 +1,23 @@
+// Genel değişken kullanımı için
+// Lokal Global farkı yoktur
+// Değişken sonradan değiştirilebilir
+var ad = 'ibrahim'
+ad = 'Halil İbrahim'
+
+// Lokal Global farkı vardır
+// Değişken sonradan değiştirilebilir
+let soyad = 'Karabulut'
+soyad = 'Blackcloud'
+
+// Lokal Global farkı vardır
+// Degğişken sonradan değiştirilemez
+const dogumTarihi = 1999
+
+// Çoklu değişken basımı için
+let lise = true,
+  universite = true
+
+// 1.Değişken basma yöntemi
+console.log('Merhaba benim adım '+ad+' Soyadım '+soyad+' Dogum Yılım: '+dogumTarihi)
+// 2.Template string yöntemi
+console.log(`Merhaba benim adım ${ad} Soyadım ${soyad} Dogum Yılım: ${dogumTarihi}`)
