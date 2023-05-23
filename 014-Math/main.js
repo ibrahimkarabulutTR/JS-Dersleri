bd16ca5e5e292b63aa1bf63225e06992af174d4b
İbrahim Karabulut <job.ibrahimkarabulut@gmail.com>
2023-05-23 15:44:09 +0300

    14-Math İslendi

---
+++ 014-Math/main.js
@@ -0,0 +1,22 @@
+console.log(Math)
+
+//Pi değeri bastırmak için
+console.log(Math.PI)
+
+//Sayıyı en yakına yuvarlamak için
+console.log(Math.round(5.3))
+let yuvarla = 8.9
+console.log(Math.round(yuvarla))
+
+//Sayıyı bir alt kata yuvarlamak için
+console.log(Math.floor(5.9))
+
+//Sayıyı bir üst kata yuvarlamak için
+console.log(Math.ceil(5.1))
+
+//Sayının ondalığını silmek için
+console.log(Math.trunc(5.1))
+
+//Rastgele sayı oluşturmak için
+console.log(Math.random())
+console.log(Math.round(Math.random()*125))
