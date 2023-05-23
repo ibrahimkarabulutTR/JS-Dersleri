54d2c51bde3cb65d4cb398bc3190f8aff21df50c
İbrahim Karabulut <job.ibrahimkarabulut@gmail.com>
2023-05-23 13:36:04 +0300

    4-Number İslendi

---
+++ 004-Number/main.js
@@ -0,0 +1,33 @@
+/*
+
++ ->Toplama
+- ->Çıkarma
+/ ->Bölme
+% ->Kalan
+* ->Çarpma
+** ->Üssü
+
+*/
+
+//Matematiksel İfadeler
+console.log("6 + 12 =", 6 + 12);
+console.log("12 - 7 =", 12 - 7);
+console.log("8 * 3 =", 8 * 3);
+console.log("3 ** 3 =",3 ** 3);
+console.log("81 / 9 =",81 / 9);
+console.log("89 % 9 =",89 % 9);
+
+//Mantıksal İfadeler
+let sayi = 25;
+sayi = sayi+1;
+sayi += 1;
+sayi++;
+console.log(sayi);
+
+let kalansayi = 37;
+kalansayi%=3;
+console.log(kalansayi);
+
+let katsayi = 3;
+katsayi**=3;
+console.log(katsayi);
