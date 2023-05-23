0905f592430348e778acada26389808729d5f59a
İbrahim Karabulut <job.ibrahimkarabulut@gmail.com>
2023-05-23 16:46:47 +0300

    19-CssDüzenleme İslendi

---
+++ 019-EditCSS/main.js
@@ -0,0 +1,10 @@
+const h1 = document.querySelector('h1#head')
+
+//h1.setAttribute('style', 'background:blue')
+//bu şekilde yazılırsa üzerine yazar
+
+//console.log(h1.style)
+h1.style.background="blue"
+h1.style.margin="0"
+h1.style.textAlign="center"
+h1.style.padding="50px"
