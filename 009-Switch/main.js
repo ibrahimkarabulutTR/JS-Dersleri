250c73164551a42bcb5485329a800491bd4c1346
İbrahim Karabulut <job.ibrahimkarabulut@gmail.com>
2023-05-23 14:44:55 +0300

    9-Switch İslendi

---
+++ 009-Switch/main.js
@@ -0,0 +1,26 @@
+let sonuc = 'aa';
+// Kullanılacak değişken
+switch(sonuc){
+		// Koşul
+		case 'aa':
+				// Görev
+				console.log("Notunuz aa");
+				// Sonlandırma Komutu
+				break;
+		case 'ba':
+				console.log("Notunuz b");
+				break;
+		case 'ab':
+				console.log("Notunuz ab");
+				break;
+		case 'cb':
+				console.log("Notunuz cb");
+				break;
+		case 'cc':
+				console.log("Notunuz cc");
+				break;
+		// Varsayılan
+		default:
+				console.log("Geçemediniz");
+				break;                                  
+}
